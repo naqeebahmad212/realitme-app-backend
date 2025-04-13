d942b988b4f7c63899bc88f2e4f266e7cde4784b
naqeeb <naqeebahmad212@gmail.com>
2025-04-13 10:37:19 +0500

    cors added

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const server = require("http").createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://chat-frontend-beige.vercel.app",
+    origin: "https://chat-flame-alpha.vercel.app",
     methods: ["GET", "POST"],
     allowedHeaders: ["*"],
   },
